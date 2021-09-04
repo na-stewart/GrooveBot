7699eb428644990070f27c651be6f9475a556e68
Sunset Dev <aidanstewart@sunsetdeveloper.com>
2021-09-04 18:30:43 -0400

    Method renaming

--- groove.py
+++ groove.py
@@ -20,7 +20,7 @@ async def on_ready():
     print('Groovebot initialized.')
 
 
-async def on_member_event(member, file):
+async def read_on_member_event(member, file):
     channel = member.guild.get_channel(int(config['GROOVE']['general_channel_id']))
     response = random.choice(await read_file(file, as_array=True)).format(member.mention, member.name,
                                                                           member.discriminator)
@@ -29,13 +29,13 @@ async def on_member_event(member, file):
 
 @bot.event
 async def on_member_join(member):
-    await on_member_event(member, 'greetings.txt')
+    await read_on_member_event(member, 'greetings.txt')
     await member.send(await read_file('welcome.txt'))
 
 
 @bot.event
 async def on_member_remove(member):
-    await on_member_event(member, 'farewells.txt')
+    await read_on_member_event(member, 'farewells.txt')
 
 
 async def handle_invalid_command(ctx, error):
