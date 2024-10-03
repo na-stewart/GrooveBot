a02344dfd5bee2a88af92f80fbe4ea6d70045633
na-stewart <na.stewart365@gmail.com>
2024-10-02 22:42:50 -0400

    Permissions revision

--- groove.py
+++ groove.py
@@ -87,7 +87,11 @@ async def response(
     await ctx.respond(response_message, embed=embed)
 
 
-@album_group.command(name="list", description="List all available albums.",  default_member_permissions=None)
+@album_group.command(
+    name="list",
+    description="List all available albums.",
+    default_member_permissions=None,
+)
 async def list_albums(ctx: discord.ApplicationContext):
     albums = await Album.all()
     if albums:
